d5efefacb3ecc4f1fad0337f0eac6322a89ca167
ShrutiChavan123 <shruchavan123@gmail.com>
2024-02-26 11:16:27 +0530

    print integer in java using loop

---
+++ javapractice/src/javapractice/Integers.java
@@ -0,0 +1,13 @@
+package javapractice;
+
+public class Integers {
+
+	public static void main(String[] args) {
+		int a;    //declaring variable
+		for(a=1;a<=10;a++) {
+			System.out.println(a);
+		}
+
+	}
+
+}
