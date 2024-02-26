8272d84717929019757dffa43599fb2710c6cbdf
ShrutiChavan123 <shruchavan123@gmail.com>
2024-02-26 11:29:04 +0530

    Conversion of Fahrenheit to Celsius

---
+++ javapractice/src/javapractice/FahrenheitToCelsius.java
@@ -0,0 +1,20 @@
+package javapractice;
+
+import java.util.Scanner;
+
+public class FahrenheitToCelsius {
+
+	public static void main(String[] args) {
+		float temperature;
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter temperature in Fahrenheit:");
+		temperature = sc.nextFloat();
+		
+		temperature =((temperature-32)*5/9);
+		
+		System.out.println("Temperature in Celsius = "+temperature);
+
+	}
+
+}
